08696825734dcd64b0de9fce02129551a0b448a0
Borja <abal.varela@gmail.com>
2022-06-16 06:33:01 +0200

    Actualizaciones menu

--- src/app/frames/frame-mes/mostrar-ingresos/model-add-ingresos/model-add-ingresos.component.ts
+++ src/app/frames/frame-mes/mostrar-ingresos/model-add-ingresos/model-add-ingresos.component.ts
@@ -1,16 +1,18 @@
-import {Component} from '@angular/core';
+import {Component, Input, OnInit } from '@angular/core';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { AdministracionApiService } from 'src/app/administracion-api.service';
+
 
 @Component({
   selector: 'app-model-add-ingresos',
   templateUrl: './model-add-ingresos.component.html',
   styleUrls: ['./model-add-ingresos.component.scss']
 })
-export class ModelAddIngresosComponent {
+export class ModelAddIngresosComponent implements OnInit {
 
   closeResult = '';
 
-  constructor(private modalService: NgbModal) {}
+  constructor(private modalService: NgbModal, private service:AdministracionApiService) {}
 
   open(content) {
     this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
@@ -18,6 +20,32 @@ export class ModelAddIngresosComponent {
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
     });
+  }
+
+  @Input() movimiento:any;
+
+
+  ngOnInit(): void {
+
+  }
+
+  addIngreso(){
+    var miNombre = <HTMLInputElement>document.getElementById("nombre")
+    
+    var miValor = <HTMLInputElement>document.getElementById("valor")
+    
+    this.movimiento = {
+      nombre: miNombre.value,
+      valor: miValor.value,
+      fecha: new Date,
+      idUsuario: 1,
+      idCategoria: 1,
+      mes: 1,
+      año: 2022,
+      idTipoGasto: 1
+    }
+
+    this.service.addMovimiento(this.movimiento);
   }
 
   private getDismissReason(reason: any): string {
