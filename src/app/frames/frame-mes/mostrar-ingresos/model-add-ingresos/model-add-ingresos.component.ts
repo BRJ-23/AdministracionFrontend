d955d0c5fe9d9249d747f29a170ab1c08220f323
Borja <abal.varela@gmail.com>
2022-06-16 03:43:12 +0200

    Ventana de añadir ingreso

--- src/app/frames/frame-mes/mostrar-ingresos/model-add-ingresos/model-add-ingresos.component.ts
+++ src/app/frames/frame-mes/mostrar-ingresos/model-add-ingresos/model-add-ingresos.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-model-add-ingresos',
   templateUrl: './model-add-ingresos.component.html',
   styleUrls: ['./model-add-ingresos.component.scss']
 })
-export class ModelAddIngresosComponent implements OnInit {
+export class ModelAddIngresosComponent {
 
-  constructor() { }
+  closeResult = '';
 
-  ngOnInit(): void {
+  constructor(private modalService: NgbModal) {}
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
   }
 
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+  guardarIngreso
 }
